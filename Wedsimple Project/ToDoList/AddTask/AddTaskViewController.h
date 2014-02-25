bccfa94fcc1cfa7b13ad923c547c4c78ba2d2fc9
sumannaskar <micronixsystem@Micronixs-MacBook-Pro.local>
2014-02-25 18:06:54 +0530

    commit

--- Wedsimple Project/ToDoList/AddTask/AddTaskViewController.h
+++ Wedsimple Project/ToDoList/AddTask/AddTaskViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface AddTaskViewController : UIViewController
+{
+    IBOutlet UIScrollView *scroll;
+}
 
 @end
--- Wedsimple Project/ToDoList/AddTask/AddTaskViewController.m
+++ Wedsimple Project/ToDoList/AddTask/AddTaskViewController.m
@@ -27,6 +27,7 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    scroll.contentSize=CGSizeMake(320, 500);
 }
 
 - (void)didReceiveMemoryWarning
