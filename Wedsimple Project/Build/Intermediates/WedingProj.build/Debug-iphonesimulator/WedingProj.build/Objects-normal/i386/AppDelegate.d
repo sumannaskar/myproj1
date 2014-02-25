8bfd11309f3d7c937703aa68622457300a171043
sumannaskar <micronixsystem@Micronixs-MacBook-Pro.local>
2014-02-25 13:08:38 +0530

    jhgj

--- Wedsimple Project/Build/Intermediates/WedingProj.build/Debug-iphonesimulator/WedingProj.build/Objects-normal/i386/AppDelegate.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/macmini2/Google\ Drive/Wedsimple\ Project/WedingProj/AppDelegate.m \
-  /Users/macmini2/Google\ Drive/Wedsimple\ Project/WedingProj/AppDelegate.h \
-  /Users/macmini2/Google\ Drive/Wedsimple\ Project/Welcome/WelcomeViewController.h
