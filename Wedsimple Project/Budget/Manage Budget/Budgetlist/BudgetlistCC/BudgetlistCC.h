46574f78ba19b5d0a4aea4607c8ec9a7c2e40648
suman naskar <suman.cse.imps@gmail.com>
2014-02-25 15:40:09 +0530

    add BudgetlistCC ui

---
+++ Wedsimple Project/Budget/Manage Budget/Budgetlist/BudgetlistCC/BudgetlistCC.h
@@ -0,0 +1,13 @@
+//
+//  BudgetlistCC.h
+//  WedingProj
+//
+//  Created by macmini2 on 2/25/14.
+//  Copyright (c) 2014 Micronix Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BudgetlistCC : UITableViewCell
+
+@end
