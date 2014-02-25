cea29b98709caa7a8ca0104237a4043580f58b2a
sumannaskar <suman.cse.imps@gmail.com>
2014-02-25 15:34:41 +0530

    add string

--- Wedsimple Project/Guest/GuestViewController.m
+++ Wedsimple Project/Guest/GuestViewController.m
@@ -41,6 +41,7 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
+    NSString *a;
     return 10;
 }
 //for normal table view....
