d886043da27c1bc9b69a2f47c87c1424e98c0f8b
sumannaskar <suman.cse.imps@gmail.com>
2014-02-26 13:35:47 +0530

    commit

---
+++ Wedsimple Project/Guest/AddGuest/AddGuestViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddGuestViewController.h
+//  WedingProj
+//
+//  Created by Micronixtraining on 2/26/14.
+//  Copyright (c) 2014 Micronix Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddGuestViewController : UIViewController
+
+@end
---
+++ Wedsimple Project/Guest/AddGuest/AddGuestViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddGuestViewController.m
+//  WedingProj
+//
+//  Created by Micronixtraining on 2/26/14.
+//  Copyright (c) 2014 Micronix Technologies. All rights reserved.
+//
+
+#import "AddGuestViewController.h"
+
+@interface AddGuestViewController ()
+
+@end
+
+@implementation AddGuestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
